2f1a1557a1200713b129aa96bfa8a911e7502185
BilgCastro <bilgcamposcs#gmail.com>
2020-08-20 15:44:43 +0100

    Adicionei todos arquivos

---
+++ exemplo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo simples de Drag and Drop</title>
+</head>
+<body>
+    
+
+    <script src="alljquery/external/jquery/jquery.js"></script>
+    <script src="alljquery/jquery-ui.js"></script>
+    <script src="exemplo.js"></script>
+</body>
+</html>
